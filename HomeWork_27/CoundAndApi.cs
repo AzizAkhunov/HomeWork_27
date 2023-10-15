43f3e84b24906a341fd56234b0bd952ec56b8a0d
AkhunovAziz <akhunovaziz735@gmail.com>
2023-10-15 18:16:02 +0500

    Добавьте файлы проекта.

---
+++ HomeWork_27/CoundAndApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_27
+{
+    public class CountAndApi
+    {
+
+        public string count { get; set; }
+        public List<APIs> entries { get; set; }
+    }
+}
